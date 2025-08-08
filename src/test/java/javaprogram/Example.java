feecf14f7e46842b8d3a46661f9145811f822563
Rahini36 <rahiniperiyasamy1304@gmail.com>
2025-08-08 20:33:15 +0530

    updated Example class

---
+++ src/test/java/javaprogram/Example.java
@@ -0,0 +1,30 @@
+package javaprogram;
+
+public class Example {
+	public static void main(String[]args) {
+		
+			String a="abc11b3jh";
+			int sum=0;
+			StringBuilder currentnumber=new StringBuilder();
+			
+			for(char s:a.toCharArray()) {
+				if(Character.isDigit(s)) {
+					currentnumber.append(s);
+				}else {
+					if(currentnumber.length()>0) {
+						sum=sum+Integer.parseInt(currentnumber.toString());
+						currentnumber.setLength(0);
+						
+					}
+				}
+				if(currentnumber.length()>0) {
+					sum=sum+Integer.parseInt(currentnumber.toString());
+				}
+			}
+				
+			
+			System.out.println(sum);
+	}
+
+}
+ 
